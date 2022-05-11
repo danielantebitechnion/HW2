1029c8a7131a59a6ab2c6afe55264bb72cc942ac
danielantebitechnion <103208886+danielantebitechnion@users.noreply.github.com>
2022-05-11 17:16:39 +0300

    Update Mtmchkin.cpp

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -1,10 +1,10 @@
 #include "Mtmchkin.h"
 
-
+#define MAX_LEVEL 10
 int Mtmchkin::m_currentCard = 0;
 
-Mtmchkin::Mtmchkin(const char* playerName, const Card* cardsArray, int numOfCards){
-    this->m_player(playerName);
+Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCards){
+    this->m_player = Player(playerName);
     this->m_gameStatus = GameStatus::MidGame;
     this->m_numOfCards = numOfCards;
     this->m_cardsArray = new Card[numOfCards];
@@ -12,22 +12,52 @@ Mtmchkin::Mtmchkin(const char* playerName, const Card* cardsArray, int numOfCard
         this->m_cardsArray[i] = cardsArray[i];
     }
 }
+Mtmchkin::Mtmchkin(){
+    m_cardsArray = NULL;
+    m_gameStatus = GameStatus::MidGame;
+    m_player = NULL;
+    m_currentCard = -1;
+    m_numOfCards =0;
+
+}
+
+Mtmchkin::~Mtmchkin(){
+    delete[] m_cardsArray;
+}
+
+Mtmchkin::Mtmchkin(const Mtmchkin& mtm):
+    m_cardsArray(new Card[m_numOfCards]){
+        for(int i=0;i<m_numOfCards; ++i){
+            m_cardsArray[i] = this->m_cardsArray[i];
+        }
+    }
+
+
+Mtmchkin::Mtmchkin(const char *playerName){
+    m_player = Player(playerName);
+}
 
 void Mtmchkin::playNextCard(){
     Card curentCard = m_cardsArray[m_currentCard];
     curentCard.printInfo();
     curentCard.applyEncounter(m_player);
     m_player.printInfo(); 
-    if(m_currentCard == m_numOfCards-1){//last card on pack
+    if(m_currentCard >= m_numOfCards-1){//last card on pack
         m_currentCard =0;
     }
     else{
-        m_currentCard++;
+        ++m_currentCard;
+    }
+    if(m_player.getLevel()>=MAX_LEVEL){
+        m_gameStatus = GameStatus::Win;
+    }
+    else if(m_player.isKnockedOut()){
+        m_gameStatus = GameStatus::Loss;
     }
 }
 
-bool Mtmchkin::isOver(){
-    if(m_gameStatus==GameStatus::Loss ||m_gameStatus==GameStatus::Win){
+bool Mtmchkin::isOver() const{
+    if(getGameStatus() == GameStatus::Loss || getGameStatus()==GameStatus::Win){
         return true;
     }
     return false;
