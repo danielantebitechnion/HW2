19fb49e176d370bd608337ea454fe33f4cc7ebce
danielantebitechnion <103208886+danielantebitechnion@users.noreply.github.com>
2022-05-14 12:07:02 +0300

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,10 +1,8 @@
 #include "Card.h"
 #include "utilities.h"
 
-Card::Card(CardType type, const CardStats& stats){
-    this->m_effect = type;
-    this->m_stats = stats;
-}
+Card::Card(CardType type, const CardStats& stats) 
+: m_effect(type),m_stats(stats){}
 
 void Card::applyEncounter(Player& player) const{
     switch(m_effect){
